bd74e04994fdeccad594bf5b34a2665ffaaa7c40
leodorna <leonardo.dorneles@inf.ufrgs.br>
2016-07-03 12:45:28 -0300

    [sei la se funciona~]

--- release_state.vhd
+++ release_state.vhd
@@ -8,19 +8,24 @@ port (
   refri: in std_logic;
   suco: in std_logic;
   saldo: in std_logic_vector(11 down to 0);
+  led: out std_logic;
 );
 end buy_state;
 
 architecture behav of diplay_tester is
 
-process(clock, drink)
-begin
-  if(rising_edge(clock)) then
-	 if(refri = '1') then
-      saldo <= saldo - "000010010110";    
-   elsif(suco = '1') then
-      saldo <= saldo - "00001001011";
-   end if;
-  end if;
-
+  process(clock, drink)
+    begin
+      if(rising_edge(clock)) then
+    	 if(refri = '1') then
+          saldo <= saldo - "000010010110";
+       elsif(suco = '1') then
+          saldo <= saldo - "00001001011";
+       end if;
+       led <= '1';
+      end if;
+      if(rising_edge(clock)) then
+        led <= '0';
+      end if;
+  end process;
 end behav;
